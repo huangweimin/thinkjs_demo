127053b7c25c525d654299e789049e488f27fe93
hwm <hwm@gmail.com>
2016-08-05 13:59:10 +0800

    添加文章数据

--- app/admin/controller/product.js
+++ app/admin/controller/product.js
@@ -9,11 +9,33 @@ module.exports = think.controller(Base, {
    */
    addAction: function(self){
 
-     console.log('in')
-     
-     this.model('article').add({title: "xxx", content: "yyy",catalogId:"1"}).then(result=>{
+     return this.display();
+
+   },
+
+   iaddAction: function(self){
+
+     var allParams = this.post();
+     if(allParams._name != '' && allParams._intro != '' && allParams._catalogId != ''){
+       this.model('article').add({title: allParams._name ,content:allParams._intro ,catalogId:allParams._catalogId}).then(result=>{
+         return this.json({success: true , msg : "添加成功"});
+       }).catch(e=>{
+         return this.json({success: false , msg : e.message});
+       })
+     }else{
+       return this.json({success: false , msg : "必须传递参数"});
+     }
+   },
+
+   listAction :function(self){
+
+     this.model('article').select().then(result=>{
+       this.assign({
+         list:result
+       })
        return this.display();
      })
 
    }
+
 });
